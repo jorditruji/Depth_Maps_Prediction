a72299e49f13b21875fab41055893b98a83d671b
jorditruji <you@example.com>
2019-01-28 20:55:44 +0100

    debugging pspnet

--- Models/extractors.py
+++ Models/extractors.py
@@ -142,6 +142,7 @@ class ResNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
+        print(type(x))
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
